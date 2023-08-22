36d58bba5f8e754d77ce5b23edcd0a4cdffc3bb3
RngValue <104089072+RngValue@users.noreply.github.com>
2023-08-22 19:23:46 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,18 @@ use sysinfo::{CpuExt, System, SystemExt};
 //NetworkExt, NetworksExt, ProcessExt, CpuExt, 
 fn main() {
     let mut sys = System::new_all();
+    let mut sys_name = format!("{:?}", sys.name());
+    sys_name = str::replace(&sys_name, "Some(\"","");
+    sys_name = str::replace(&sys_name, "\")","");
     loop {
         if Command::new("clear").status().unwrap().success() {
             sys.refresh_all();
-            println!("System name: {:?}", sys.name());
+            println!("System name:\t{:?}", sys_name);
             for cpu in sys.cpus() {
-                println!("{} usage: {} %;", cpu.name(), cpu.cpu_usage());
+                println!("{} usage:\t{} %;", cpu.name(), cpu.cpu_usage());
             }
-            println!("RAM: {} MB", sys.total_memory()/1048576);
-            println!("RAM usage: {} MB", sys.used_memory()/1048576);
+            println!("RAM capacity:\t{} MB", sys.total_memory()/1048576);
+            println!("RAM usage:\t{} MB", sys.used_memory()/1048576);
             println!("press ctr+c");
             std::thread::sleep(std::time::Duration::from_millis(1000));
         }
