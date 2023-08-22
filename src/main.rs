f0c79982bc11074c7770f5fd1a0c7518330edc43
RngValue <104089072+RngValue@users.noreply.github.com>
2023-08-22 19:41:56 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,13 @@ fn main() {
     let mut sys_name = format!("{:?}", sys.name());
     sys_name = str::replace(&sys_name, "Some(\"","");
     sys_name = str::replace(&sys_name, "\")","");
+    let mut sys_ver = format!("{:?}", sys.os_version());
+    sys_ver = str::replace(&sys_ver, "Some(\"","");
+    sys_ver = str::replace(&sys_ver, "\")","");
     loop {
         if Command::new("clear").status().unwrap().success() {
             sys.refresh_all();
-            println!("System name:\t{:?}", sys_name);
+            println!("System name:\t{} {}", sys_name, sys_ver);
             for cpu in sys.cpus() { println!("{} usage:\t{} %", cpu.name(), cpu.cpu_usage()); }
             println!("RAM capacity:\t{} MB", sys.total_memory()/1048576);
             println!("RAM usage:\t{} MB", sys.used_memory()/1048576);
