af99bf885952a292d50d6cf7dfa6b0b3312a2c21
RngValue <104089072+RngValue@users.noreply.github.com>
2023-08-22 18:44:41 +0200

    rust

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::process::Command;
+use sysinfo::{CpuExt, System, SystemExt, };
+//NetworkExt, NetworksExt, ProcessExt, CpuExt, 
+fn main() {
+    let mut sys = System::new_all();
+    
+    let mut a = 0;
+    loop {
+        if Command::new("clear").status().unwrap().success() {
+            a += 1;
+            sys.refresh_all();
+            println!("System name: {:?}", sys.name());
+            for cpu in sys.cpus() {
+                println!("{} usage: {} %;", cpu.name(), cpu.cpu_usage());
+            }
+            println!("RAM: {} MB", sys.total_memory()/1048576);
+            println!("RAM usage: {} MB", sys.used_memory()/1048576);
+            println!("press ctr+c");
+            std::thread::sleep(std::time::Duration::from_millis(1000));
+        }
+    }
+}
